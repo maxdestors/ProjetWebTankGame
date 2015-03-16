e3d97b837c31e18edbd0c0ad4f9f798ffd08c6b6
Romain Palmero <mesprojetsinfo@gmail.com>
2015-03-16 15:21:57 +0100

    Map

--- client/js/classes/map.js
+++ client/js/classes/map.js
@@ -4,21 +4,5 @@
 
 var Map = function ()
 {
-    var ctx;
-
-    var init = function (newctx) {
-        ctx = newctx;
-    };
-
-    function draw() {
-        ctx.save();
-        ctx.fillStyle = 'black';
-        ctx.fillRect(200, 200, 100, 200);
-        ctx.restore();
-    }
-
-    return {
-        init: init,
-        draw: draw
-    }
+    // TODO
 };
